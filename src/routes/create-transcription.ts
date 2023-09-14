76b4f1cbfe3a90072b737228c05b60e31772f677
Lucas Marchiori <sinonsrt@hotmail.com>
2023-09-14 00:25:11 -0300

    add openai lib

--- src/routes/create-transcription.ts
+++ src/routes/create-transcription.ts
@@ -1,5 +1,7 @@
 import { FastifyInstance } from 'fastify';
+import { createReadStream } from 'node:fs';
 import { z } from 'zod';
+import { prisma } from '../lib/prisma';
 
 export const createTranscriptionRoutes = async (app: FastifyInstance) => {
   app.post('/videos/:videoId/transcription', async (request) => {
@@ -8,5 +10,24 @@ export const createTranscriptionRoutes = async (app: FastifyInstance) => {
     });
 
     const { videoId } = paramsSchema.parse(request.params);
+
+    const bodySchema = z.object({
+      prompt: z.string(),
+    });
+
+    const { prompt } = bodySchema.parse(request.body);
+
+    const video = await prisma.video.findUniqueOrThrow({
+      where: {
+        id: videoId,
+      },
+    });
+
+    const audioVideoStream = createReadStream(video.path);
+
+    return {
+      videoId,
+      prompt,
+    };
   });
 };
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
+import dotenv from 'dotenv';
 import { fastify } from 'fastify';
 import { getAllPromptRoute } from './routes/get-all-prompts';
 import { uploadVideoRoute } from './routes/upload-video';
+import { createTranscriptionRoutes } from './routes/create-transcription';
+
+dotenv.config();
 
 const app = fastify();
 
 app.register(getAllPromptRoute);
 app.register(uploadVideoRoute);
+app.register(createTranscriptionRoutes);
 
 app.get('/', () => {
   return 'Hello World';
