ea3f7bfaf12d89d9acb390a5b0cc3624fbeee01b
Lucas Marchiori <sinonsrt@hotmail.com>
2023-09-14 11:04:26 -0300

    implement ai lib to treat response with stream

---
+++ prisma/seed.ts
@@ -0,0 +1,44 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.prompt.deleteMany();
+
+  await prisma.prompt.create({
+    data: {
+      title: 'Título YouTube',
+      template: `Seu papel é gerar três títulos para um vídeo do YouTube.
+
+Abaixo você receberá uma transcrição desse vídeo, use essa transcrição para gerar os títulos.
+Abaixo você também receberá uma lista de títulos, use essa lista como referência para os títulos a serem gerados.
+
+Os títulos devem ter no máximo 60 caracteres.
+Os títulos devem ser chamativos e atrativos para maximizar os cliques.
+
+Retorne APENAS os três títulos em formato de lista como no exemplo abaixo:
+'''
+- Título 1
+- Título 2
+- Título 3
+'''
+
+Transcrição:
+'''
+{transcription}
+'''`.trim(),
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
+
+// prisma db seed
--- src/routes/generate-ai-completion.ts
+++ src/routes/generate-ai-completion.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from 'fastify';
 import { z } from 'zod';
+import { streamToResponse, OpenAIStream } from 'ai';
 import { prisma } from '../lib/prisma';
 import { openai } from '../lib/openai';
 
@@ -7,11 +8,11 @@ export const generateAICompletionRoute = async (app: FastifyInstance) => {
   app.post('/ai/complete', async (request, reply) => {
     const bodySchema = z.object({
       videoId: z.string().uuid(),
-      template: z.string(),
+      prompt: z.string(),
       temperature: z.number().min(0).max(1).default(0.5),
     });
 
-    const { template, temperature, videoId } = bodySchema.parse(request.body);
+    const { prompt, temperature, videoId } = bodySchema.parse(request.body);
 
     const video = await prisma.video.findUniqueOrThrow({
       where: {
@@ -25,7 +26,7 @@ export const generateAICompletionRoute = async (app: FastifyInstance) => {
         .send({ error: 'Video transcription was not generated yeat!' });
     }
 
-    const promptMessage = template.replace(
+    const promptMessage = prompt.replace(
       '{transcription}',
       video.transcription
     );
@@ -34,8 +35,16 @@ export const generateAICompletionRoute = async (app: FastifyInstance) => {
       model: 'gpt-3.5-turbo-16k',
       temperature,
       messages: [{ role: 'user', content: promptMessage }],
+      stream: true,
     });
 
-    return response;
+    const stream = OpenAIStream(response);
+
+    streamToResponse(stream, reply.raw, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELTE, OPTIONS',
+      },
+    });
   });
 };
