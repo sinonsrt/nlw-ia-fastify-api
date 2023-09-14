c60b8436fb752fb9b9e82c76ea9d330ef71a42f6
Lucas Marchiori <sinonsrt@hotmail.com>
2023-09-14 00:29:01 -0300

    create transcription route

--- src/routes/create-transcription.ts
+++ src/routes/create-transcription.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify';
 import { createReadStream } from 'node:fs';
 import { z } from 'zod';
 import { prisma } from '../lib/prisma';
+import { openai } from '../lib/openai';
 
 export const createTranscriptionRoutes = async (app: FastifyInstance) => {
   app.post('/videos/:videoId/transcription', async (request) => {
@@ -25,9 +26,24 @@ export const createTranscriptionRoutes = async (app: FastifyInstance) => {
 
     const audioVideoStream = createReadStream(video.path);
 
-    return {
-      videoId,
-      prompt,
-    };
+    const transcription = await openai.audio.transcriptions.create({
+      file: audioVideoStream,
+      model: 'whisper-1',
+      language: 'pt',
+      response_format: 'json',
+      temperature: 0,
+      prompt: prompt,
+    });
+
+    await prisma.video.updateMany({
+      where: {
+        id: videoId,
+      },
+      data: {
+        transcription: transcription.text,
+      },
+    });
+
+    return transcription.text;
   });
 };
